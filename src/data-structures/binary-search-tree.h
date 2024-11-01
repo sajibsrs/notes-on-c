f4bd416c4c6f248adfc6dd2270b1e5e142e1cc86
Sajidur Rahman <sajibsrs@gmail.com>
2024-11-02 02:19:56 +0600

    added bst header

---
+++ src/data-structures/binary-search-tree.h
@@ -0,0 +1,37 @@
+#ifndef BST_H
+#define BST_H
+
+/// @brief Binary seach tree node
+typedef struct BSTNode {
+  BSTNode *next;
+  BSTNode *prev;
+  int value;
+} BSTNode;
+
+/// @brief Insert a value
+/// @param root Root node of the tree.
+/// @param value Node value.
+/// @return Pointer to the new root.
+BSTNode *bst_insert(BSTNode *root, int value);
+
+/// @brief Search for a value in the tree.
+/// @param root Root node of the tree.
+/// @param value Value to search for.
+/// @return Returns the node containing the value or NULL.
+BSTNode *bst_search(BSTNode *root, int value);
+
+/// @brief Delete a node from the tree.
+/// @param root Root node of the tree.
+/// @param value Value to delete.
+/// @return Root of the tree or NULL.
+BSTNode *bst_delete(BSTNode *root, int value);
+
+/// @brief Print the tree.
+/// @param root Root of the tree.
+void bst_print(BSTNode *root);
+
+/// @brief Delete the whole tree.
+/// @param root Root node of the tree.
+void bst_destroy(BSTNode *root);
+
+#endif
