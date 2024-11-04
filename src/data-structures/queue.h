303b9b4f74fc22ee84c1e91792ae0e8b4688bc41
Sajidur Rahman <sajibsrs@gmail.com>
2024-11-05 03:34:08 +0600

    doc style update

--- src/data-structures/queue.h
+++ src/data-structures/queue.h
@@ -1,21 +1,27 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-/// @brief Queue node type.
+/**
+ * @brief Queue node type.
+ */
 typedef struct QueueNode {
   QueueNode *next;
   int value;
 } QueueNode;
 
-/// @brief Allocate and initialize queue node.
-/// @param value Node value.
-/// @return Pointer to the newly created node.
+/**
+ * @brief Allocate and initialize queue node.
+ * @param value Node value.
+ * @return Pointer to the newly created node.
+ */
 static QueueNode *queue_node_construct(int value);
 
-/// @brief Insert into the queue.
-/// @param node Starting node (head).
-/// @param value Queue node value.
-/// @return Starting node.
+/**
+ * @brief Insert into the queue.
+ * @param node Starting node (head).
+ * @param value Queue node value.
+ * @return Starting node.
+ */
 QueueNode *queue_insert(QueueNode *node, int value);
 
 #endif
