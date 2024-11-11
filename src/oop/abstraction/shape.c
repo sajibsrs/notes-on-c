ceb845a5ecc8b1bd9ff866d166f768f66c2b5755
Sajidur Rahman <sajibsrs@gmail.com>
2024-11-11 14:45:42 +0600

    indent with 4 spaces

--- src/oop/abstraction/shape.c
+++
@@ -1,9 +0,0 @@
-#include "shape.h"
-
-float shape_area(Shape *shape) {
-    return shape->vtable->area(shape);
-}
-
-void shape_draw(Shape *shape) {
-    shape->vtable->draw(shape);
-}
--- src/oop/inheritance/shape.c
+++
@@ -1,6 +0,0 @@
-#include "shape.h"
-
-void shape_init(Shape *shape, float x, float y) {
-    shape->x = x;
-    shape->y = y;
-}
